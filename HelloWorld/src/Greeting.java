cde043959d4c7f28ac5464f1303755560e490341
emuchave <eliazardo.muchave.ac@gmail.com>
2015-01-28 12:33:27 +0200

    second

--- HelloWorld/src/Greeting.java
+++ HelloWorld/src/Greeting.java
@@ -1,9 +1,17 @@
+import java.util.Date;
+
 
 public class Greeting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.println(new Date().getTime());
+		System.out.println("Second change");
 
 	}
+	
+	
 
 }
